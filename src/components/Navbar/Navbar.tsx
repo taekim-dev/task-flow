53c8bf7bb0942557eb9e1e209181ad5918642b4b
Tae Kim <taekim.dev@gmail.com>
2023-05-26 17:16:56 +0800

    added Total task in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,13 +7,14 @@ interface NavbarProps {
 function Navbar({ username, avatar, totalTasks }: NavbarProps) {
   return (
     <div className="flex justify-between items-center p-4 bg-white border-b-2 border-gray-200">
-      <div className="text-2xl font-bold text-black-500">
+      <div className="text-2xl font-bold text-blue-400">
         TaskFlow
       </div>
+      <div className="text-xl font bold text-black-300">Total Task: {totalTasks}</div>
       <div className="flex items-center">
         <span className="text-xl font-semibold">Welcome, {username}</span>
         <img 
-          className="h-12 w-12 rounded-full mr-2" 
+          className="h-12 w-12 rounded-full mr-3" 
           src={`/img/Avatar-${avatar}.jpg`} 
           alt="User avatar"
         />
