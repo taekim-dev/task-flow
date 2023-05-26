a7ed887f30fa467188a2ad7adf556979066631bb
Tae Kim <taekim.dev@gmail.com>
2023-05-26 17:08:44 +0800

    added navbar to display logo, username, and avatar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,25 @@
 interface NavbarProps {
-    username: string;
-    avatar: number;
-    totalTasks: number;
-  }
-  
-  const Navbar: React.FC<NavbarProps> = ({ username, avatar, totalTasks }) => {
-    return (
-      <nav>
-        <header className="bg-blue-500 text-white text-lg p-4">
-            <h1 className="font-bold">TaskFlow</h1>
-        </header>
-        <div>Logo</div>
-        <div>{username}</div>
-        <div>Total tasks: {totalTasks}</div>
-        <img src={`/path/to/avatars/avatar-${avatar}.jpg`} alt={`${username}'s avatar`} />
-      </nav>
-    );
-  }
-  
-  export default Navbar;
-  
+  username: string;
+  avatar: number;
+  totalTasks: number;
+}
+
+function Navbar({ username, avatar, totalTasks }: NavbarProps) {
+  return (
+    <div className="flex justify-between items-center p-4 bg-white border-b-2 border-gray-200">
+      <div className="text-2xl font-bold text-black-500">
+        TaskFlow
+      </div>
+      <div className="flex items-center">
+        <span className="text-xl font-semibold">Welcome, {username}</span>
+        <img 
+          className="h-12 w-12 rounded-full mr-2" 
+          src={`/img/Avatar-${avatar}.jpg`} 
+          alt="User avatar"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
