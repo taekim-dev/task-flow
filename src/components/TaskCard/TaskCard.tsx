cd145be7ec14d56971e8d579c383a32159287550
Tae Kim <taekim.dev@gmail.com>
2023-05-27 01:36:35 +0800

    added taskform

--- src/components/TaskCard/TaskCard.tsx
+++ src/components/TaskCard/TaskCard.tsx
@@ -1,4 +1,6 @@
 import { Task } from '../../types';
+import { useState } from 'react';
+import TaskForm from '../TaskForm/TaskForm'; // import TaskForm component
 
 interface TaskCardProps {
   task: Task;
@@ -7,22 +9,33 @@ interface TaskCardProps {
 }
 
 function TaskCard({ task, deleteTask, updateTask }: TaskCardProps) {
+  const [isEditing, setIsEditing] = useState(false);
+
+  const handleSubmit = (updatedTask: Task) => {
+    updateTask(updatedTask);
+    setIsEditing(false);
+  };
 
   return (
-    <div className="bg-white m-2 p-4 rounded-md">
-      <div className="flex mb-2">
-        {task.labels.map((label, index) => (
-          <div
-            key={index}
-            className="w-1/4 h-2 rounded mr-1"
-            style={{ backgroundColor: label }}
-          />
-        ))}
-      </div>
-      <h4 className="text-left text-lg font-bold ">{task.name}</h4>
-      <p>{task.dueDate}</p>
-      <button onClick={() => deleteTask(task.id)}>Delete</button>
-      <button onClick={() => updateTask({ ...task, name: 'Updated name' })}>Update</button>
+    <div className="bg-white m-2 p-4 rounded-md" onClick={() => setIsEditing(true)}>
+      {isEditing ? (
+        <TaskForm initialData={task} onSubmit={handleSubmit} onCancel={() => setIsEditing(false)} />
+      ) : (
+        <>
+          <div className="flex mb-2">
+            {task.labels.map((label, index) => (
+              <div
+                key={index}
+                className="w-1/4 h-2 rounded mr-1"
+                style={{ backgroundColor: label }}
+              />
+            ))}
+          </div>
+          <h4 className="text-left text-lg font-bold ">{task.name}</h4>
+          <p>{task.dueDate}</p>
+          <button onClick={(e) => { e.stopPropagation(); deleteTask(task.id); }}>Delete</button>
+        </>
+      )}
     </div>
   );
 }
--- src/components/TaskForm/TaskForm.tsx
+++ src/components/TaskForm/TaskForm.tsx
@@ -2,49 +2,89 @@ import { Task, LabelColor } from '../../types';
 import { useState } from 'react';
 
 interface TaskFormProps {
-  initialData?: Task; // We make this optional as it won't be necessary when adding a new task
+  initialData?: Task;
   onSubmit: (task: Task) => void;
+  onCancel: () => void;
 }
 
-function TaskForm({ initialData, onSubmit }: TaskFormProps) {
-  // We can use React's useState hook to manage the form input state
+function TaskForm({ initialData, onSubmit, onCancel }: TaskFormProps) {
   const [name, setName] = useState(initialData?.name || '');
+  const [labels, setLabels] = useState(initialData?.labels || []);
   const [description, setDescription] = useState(initialData?.description || '');
   const [dueDate, setDueDate] = useState(initialData?.dueDate || '');
-  const [labels, setLabels] = useState(initialData?.labels || []); // New label field
-  
+
   const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
-    
-    // We will call the onSubmit function with the task data
+
     onSubmit({
-      id: initialData?.id || 'newId', // Generate a new ID for a new task
+      id: initialData?.id || 'newId',
       name,
       labels,
       description,
       dueDate,
-      status: initialData?.status || 'To Do', // Default status for a new task
+      status: initialData?.status || 'To Do',
     });
   };
-  
-  return (
-    <form onSubmit={handleSubmit}>
-      {/* Add your form fields here. They should update the respective state variables on change. */}
-      {/* Here is an example for a label field. You might need to adjust it according to your needs. */}
-      <div>
-        <label>Label:</label>
-        <select multiple={true} value={labels} onChange={(e) => {
-        const selectedLabels = Array.from(e.target.selectedOptions).map((option) => option.value as LabelColor);
-        setLabels(selectedLabels);
-        }}>
-        {Object.values(LabelColor).map(color => (
-            <option key={color} value={color}>{color}</option>
-        ))}
-        </select>
 
-      </div>
-      {/* Continue with other fields... */}
-    </form>
+  return (
+    <div 
+      className="fixed top-0 left-0 w-screen h-screen flex justify-center items-center bg-black bg-opacity-50 z-10" 
+      onClick={(e) => e.stopPropagation()} // Stop the click event from propagating to the parent
+    >
+      <form
+        className="bg-white w-3/5 h-2/5 border-4 border-black shadow-xl p-4 flex flex-col justify-between"
+        onSubmit={handleSubmit}
+      >
+        <div className="flex justify-between">
+          <input 
+            className="text-2xl font-bold mb-2"
+            value={name} 
+            onChange={(e) => setName(e.target.value)} 
+          />
+          <button type="button" onClick={onCancel}>X</button>
+        </div>
+        <div className="flex justify-center space-x-4 mb-4">
+        <div className="w-1/2">
+            <label>Label:</label>
+            <select 
+            multiple={true} 
+            value={labels} 
+            onChange={(e) => {
+                const selectedLabels = Array.from(e.target.selectedOptions).map((option) => option.value as LabelColor);
+                setLabels(selectedLabels);
+            }}
+            className="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm text-base p-2"
+            >
+            {Object.entries(LabelColor).map(([colorName, colorValue]) => (
+                <option key={colorValue} value={colorValue} style={{ color: colorValue }}>
+                {colorName}
+                </option>
+            ))}
+            </select>
+        </div>
+        <div className="w-1/2">
+            <label>Due Date:</label>
+            <input
+            type="date"
+            value={dueDate}
+            onChange={(e) => setDueDate(e.target.value)}
+            className="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm text-base p-2"
+            />
+        </div>
+        </div>
+
+        <div className="mb-2">
+          <label>Description:</label>
+          <textarea 
+            value={description} 
+            onChange={(e) => setDescription(e.target.value)} 
+            maxLength={300}
+            className="w-full h-1/3 mt-1 p-2 border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm text-base"
+          />
+        </div>
+        <button className="self-end py-1 px-3 rounded bg-blue-500 text-white" type="submit">Save</button>
+      </form>
+    </div>
   );
 }
 
