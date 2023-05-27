93197ad0df431b13cbb7afec1e7bb68a61f3740b
Tae Kim <taekim.dev@gmail.com>
2023-05-27 14:49:45 +0800

    added TaskList title update

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,5 +1,6 @@
 import { Task, List } from '../../types';
 import TaskList from '../TaskList/TaskList';
+import { v4 as uuidv4 } from 'uuid';
 
 interface BoardProps {
   lists: List[];
@@ -28,17 +29,33 @@ function Board({lists, tasks, addTask, deleteTask, updateTask, addList, deleteLi
         };
     
         addTask(newTask); // Add the new task
-      };
+    };
     
-      const handleAddList = () => {
+    const handleAddList = () => {
+        // This could be a prompt, a modal, an input field, etc.
+        const newListName = window.prompt('Enter new list name');
+    
+        if (newListName === null || newListName.trim() === '') {
+          // Do nothing if the user cancels the prompt or enters an empty string
+          return;
+        }
+    
         const newList: List = {
-          id: 'newId', // Generate a new ID for the list
-          name: 'newList', // The user would input this
+          id: uuidv4(), // Generate a new unique ID for the list
+          name: newListName.trim(), // Use the user's input as the name
           tasks: [], // Initially empty
         };
     
         addList(newList); // Add the new list
-      }
+    }
+    
+    const handleUpdateListName = (listId: string, newName: string) => {
+        const updatedList = lists.find((list) => list.id === listId);
+        if (updatedList) {
+          updatedList.name = newName;
+          updateList(updatedList);
+        }
+    }
 
   return (
     <div className="bg-blue-200 flex flex-row min-h-screen overflow-auto items-start">
@@ -50,6 +67,7 @@ function Board({lists, tasks, addTask, deleteTask, updateTask, addList, deleteLi
           tasks={tasks.filter((task) => list.tasks.includes(task.id))}
           deleteTask={deleteTask}
           updateTask={updateTask}
+          updateListName={(newName: string) => handleUpdateListName(list.id, newName)} // Pass the function for updating list name
         />
       ))}
       <button onClick={handleAddList} className="bg-gray-100 w-64 m-4 rounded-xl p-4 h-12 flex justify-center items-center">
--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import TaskCard from '../TaskCard/TaskCard';
 import { Task } from '../../types';
 
@@ -8,20 +9,49 @@ interface TaskListProps {
   isAddList?: boolean;
   deleteTask: (taskId: string) => void;
   updateTask: (updatedTask: Task) => void;
+  updateListName?: (newName: string) => void;
 }
 
-function TaskList({ listTitle, tasks, onAddTask, isAddList = false, deleteTask, updateTask }: TaskListProps) {
+function TaskList({ listTitle, tasks, onAddTask, isAddList = false, deleteTask, updateTask, updateListName }: TaskListProps) {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newTitle, setNewTitle] = useState(listTitle);
+
+  const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewTitle(event.target.value);
+  };
+
+  const handleTitleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      updateListName?.(newTitle);
+      setIsEditing(false);
+    }
+  };
+
+  const handleBlur = () => {
+    updateListName?.(newTitle);
+    setIsEditing(false);
+  };
+
   return (
     <div className="bg-gray-100 w-64 m-4 rounded-xl p-4">
-      <h3 className="text-center font-bold">{listTitle}</h3>
+      {isEditing ? (
+        <input 
+          type="text" 
+          value={newTitle} 
+          onChange={handleTitleChange} 
+          onKeyPress={handleTitleKeyPress}
+          onBlur={handleBlur}
+        />
+      ) : (
+        <h3 className="text-center font-bold" onClick={() => setIsEditing(true)}>{listTitle}</h3>
+      )}
       {tasks.map((task) => (
         <TaskCard key={task.id} task={task} deleteTask={deleteTask} updateTask={updateTask} />
       ))}
       {isAddList ? (
         <button className="w-full h-12 mt-4 bg-blue-500 text-white rounded-md">+ Add another list</button>
       ) : (
-        <button onClick={onAddTask} 
-                className="w-full h-12 mt-4 border-2 border-dashed border-black rounded-md">
+        <button onClick={onAddTask} className="w-full h-12 mt-4 border-2 border-dashed border-black rounded-md">
           <div className="flex justify-center items-center h-full">
             <span className="font-bold text-xl">+</span>
             <span className="ml-2">Add a task</span>
