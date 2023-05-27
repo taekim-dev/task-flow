bfd3fedea168246278b2216971a6f32d0692caa8
Tae Kim <taekim.dev@gmail.com>
2023-05-27 17:15:31 +0800

    fix app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -55,7 +55,6 @@ function App() {
     setTasks(prevTasks => [...prevTasks, task]);
   }
 
-
   const deleteTask = (taskId: string) => {
     setTasks(prevTasks => prevTasks.filter(task => task.id !== taskId));
   }
@@ -80,19 +79,19 @@ function App() {
   return (
     <div className="App">
       {username === '' ? (
-        <LandingPage setUsername={setUsername} setAvatar={setAvatar} /> // render the LandingPage if there's no username
+        <LandingPage setUsername={setUsername} setAvatar={setAvatar} />
       ) : (
         <>
           <Navbar username={username} avatar={avatar} totalTasks={tasks.length} />
           <Board 
-          lists={lists} 
-          tasks={tasks} 
-          addTask={addTask} 
-          deleteTask={deleteTask} 
-          updateTask={updateTask} 
-          addList={addList} 
-          deleteList={deleteList} 
-          updateList={updateList} />
+            lists={lists} 
+            tasks={tasks} 
+            deleteTask={deleteTask} 
+            updateTask={updateTask} 
+            addList={addList} 
+            deleteList={deleteList} 
+            updateList={updateList} 
+          />
         </>
       )}
     </div>
