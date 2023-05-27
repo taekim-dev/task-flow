ebeee41ff0002b5c98f0cbc920c77bd88a0a9aa1
Tae Kim <taekim.dev@gmail.com>
2023-05-27 17:09:06 +0800

    add a task function

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -5,7 +5,6 @@ import { v4 as uuidv4 } from 'uuid';
 interface BoardProps {
   lists: List[];
   tasks: Task[];
-  addTask: (task: Task) => void;
   deleteTask: (taskId: string) => void;
   updateTask: (updatedTask: Task) => void;
   addList: (list: List) => void;
@@ -13,23 +12,7 @@ interface BoardProps {
   updateList: (updatedList: List) => void;
 }
 
-function Board({lists, tasks, addTask, deleteTask, updateTask, addList, deleteList, updateList}: BoardProps) {
-  
-    const onAddTask = (listId: string) => {
-        // Open a form, gather details for the new task
-        // For example, this could open a modal with a form for the user to input the new task's details
-    
-        const newTask: Task = {
-          id: 'newId', // Generate a new ID for the task
-          name: 'newTask', // The user would input this
-          labels: [], // The user would select these
-          description: 'newTaskDescription', // The user would input this
-          dueDate: '2023-01-01', // The user would input this
-          status: listId, // Initial status
-        };
-    
-        addTask(newTask); // Add the new task
-    };
+function Board({lists, tasks, deleteTask, updateTask, addList, deleteList, updateList}: BoardProps) {
     
     const handleAddList = () => {
         // This could be a prompt, a modal, an input field, etc.
@@ -62,8 +45,8 @@ function Board({lists, tasks, addTask, deleteTask, updateTask, addList, deleteLi
       {lists.map((list) => (
         <TaskList 
           key={list.id} 
-          listTitle={list.name} 
-          onAddTask={() => onAddTask(list.id)}
+          listTitle={list.name}
+          listId={list.id}
           tasks={tasks.filter((task) => list.tasks.includes(task.id))}
           deleteTask={deleteTask}
           updateTask={updateTask}
--- src/components/TaskList/TaskList.tsx
+++ src/components/TaskList/TaskList.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react';
 import TaskCard from '../TaskCard/TaskCard';
 import { Task } from '../../types';
+import TaskService from '../../utils/TaskService';
+import { v4 as uuidv4 } from 'uuid';
 
 interface TaskListProps {
   listTitle: string;
   tasks: Task[];
+  listId: string;
   onAddTask?: () => void;
   isAddList?: boolean;
   deleteTask: (taskId: string) => void;
@@ -12,9 +15,11 @@ interface TaskListProps {
   updateListName?: (newName: string) => void;
 }
 
-function TaskList({ listTitle, tasks, onAddTask, isAddList = false, deleteTask, updateTask, updateListName }: TaskListProps) {
+function TaskList({ listTitle, tasks, listId, onAddTask, isAddList = false, deleteTask, updateTask, updateListName }: TaskListProps) {
   const [isEditing, setIsEditing] = useState(false);
   const [newTitle, setNewTitle] = useState(listTitle);
+  const [newTaskName, setNewTaskName] = useState('');
+  const [showAddTaskInput, setShowAddTaskInput] = useState(false);
 
   const handleTitleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setNewTitle(event.target.value);
@@ -30,6 +35,22 @@ function TaskList({ listTitle, tasks, onAddTask, isAddList = false, deleteTask, 
   const handleBlur = () => {
     updateListName?.(newTitle);
     setIsEditing(false);
+  };
+
+  const handleAddTask = (event: React.KeyboardEvent) => {
+    if (event.key === 'Enter' && newTaskName.trim() !== '') {
+      const newTask: Task = {
+        id: uuidv4(),
+        name: newTaskName.trim(),
+        labels: [],
+        description: '',
+        dueDate: '',
+        status: listId,
+      };
+
+      TaskService.addTask(newTask);
+      setNewTaskName('');
+    }
   };
 
   return (
@@ -48,16 +69,22 @@ function TaskList({ listTitle, tasks, onAddTask, isAddList = false, deleteTask, 
       {tasks.map((task) => (
         <TaskCard key={task.id} task={task} deleteTask={deleteTask} updateTask={updateTask} />
       ))}
-      {isAddList ? (
-        <button className="w-full h-12 mt-4 bg-blue-500 text-white rounded-md">+ Add another list</button>
-      ) : (
-        <button onClick={onAddTask} className="w-full h-12 mt-4 border-2 border-dashed border-black rounded-md">
-          <div className="flex justify-center items-center h-full">
-            <span className="font-bold text-xl">+</span>
-            <span className="ml-2">Add a task</span>
-          </div>
-        </button>
+      {showAddTaskInput && (
+        <input
+          type="text"
+          placeholder="Add a task"
+          value={newTaskName}
+          onChange={(e) => setNewTaskName(e.target.value)}
+          onKeyPress={handleAddTask}
+          className="w-full h-12 mt-4 border-2 border-dashed border-black rounded-md"
+        />
       )}
+      <button onClick={() => setShowAddTaskInput(!showAddTaskInput)} className="w-full h-12 mt-4 border-2 border-dashed border-black rounded-md">
+        <div className="flex justify-center items-center h-full">
+          <span className="font-bold text-xl">+</span>
+          <span className="ml-2">Add a task</span>
+        </div>
+      </button>
     </div>
   );
 }
